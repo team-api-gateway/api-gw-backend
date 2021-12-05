37433b0038ea62f672a375ca6b193a3bace4b635
Jakob Gietl <jakob-gietl@gmx.de>
2021-12-05 19:40:14 +0100

    did go mod tidy

---
+++ TEST/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/apimanagement/armapimanagement"
+)
+
+func main() {
+	armapimanagement.NewAPIClient("1ed07f18-4c8e-424d-bd80-19e1247574ad")
+}
